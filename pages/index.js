363157d685d349cc4b91d1e62f937717290300ce
l5sistemas <luciano@l5sistemas.com.br>
2022-11-07 20:28:59 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,110 @@
+import styled from 'styled-components'
+import config from '../config.json'
+import { CSSReset } from '../src/components/CSSReset'
+import Menu from '../src/components/Menu'
+import { StyledTimeline } from '../src/components/TimeLine'
+
+
+
+function HomePage() {
+
+  const estiloHomePage = { 
+    // backgroundColor: "red" 
+  }
+
+  // console.log(config.playlist);
+
+  return (
+    <>
+    <CSSReset/>
+    <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+        <Menu/>
+        <Header />
+        <TimeLine playlists={config.playlist} />
+      </div>
+    </>
+  )
+}
+
+export default HomePage
+
+
+
+
+const StyledHeader = styled.div`
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+    .user-info{
+      display: flex;
+      align-items: center;
+      width: 100%;
+      padding: 16px 32px;
+      gap: 16px;
+      margin-top: 50px;
+    }
+  `
+
+function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner"/> */}
+
+      <section className='user-info'>
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  )
+}
+
+function TimeLine(props) {
+
+  // console.log("dentro do componente: ", props)
+
+  const playlistNames = Object.keys(props.playlists)
+
+  return (
+    <StyledTimeline>
+
+      {playlistNames.map((playlistName) => {
+
+        const videos = props.playlists[playlistName]
+        // console.log(videos)
+
+        return (
+          <section>
+            <h2>{playlistName}</h2>
+            <div>
+              {
+                videos.map(
+                  (video) => {
+                    return (
+                      <a href={video.url}>
+                        <img src={video.thumb} />
+                        <span>
+                          {video.title}
+                        </span>
+                      </a>
+                    )
+                  }
+                )
+              }
+            </div>
+          </section>
+        )
+
+      })}
+    </StyledTimeline>
+  )
+} 
